98f6b4a3152dbf98d1c07f9407e11465e4928d2c
Marcus <marcus.veres@gmail.com>
2024-09-11 17:51:29 -0400

    configures tailwindcss typography base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'xs': '0.75rem',     // 12px
+        'sm': '0.875rem',    // 14px
+        'base': ['1rem', { lineHeight: '1.5' }],      // 16px
+        'lg': ['1.625rem', { lineHeight: '1.615' }],  // 26px (p.bigger), line height 42px (42/26 ≈ 1.615)
+        'xl': ['2.625rem', { lineHeight: '1.2' }],    // 42px (h4)
+        '2xl': ['4.5rem', { lineHeight: '1.16' }],    // 72px (h3)
+        '3xl': ['6.85rem', { lineHeight: '1', letterSpacing: '-0.02em' }],    // 109.7px (h2)
+        '4xl': ['13.3125rem', { lineHeight: '1', letterSpacing: '-0.02em' }], // 213px (h1)
+      },
+    },
   },
   plugins: [],
 }
