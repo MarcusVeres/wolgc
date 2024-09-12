8d20490c3f8ec342395bd5eb15c56fad71cefa5d
Marcus <marcus.veres@gmail.com>
2024-09-11 20:47:58 -0400

    adds placeholders for all sections, with appropriate colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,8 @@ module.exports = {
         'lg': ['1.625rem', { lineHeight: '1.615' }],  // 26px (p.bigger), line height 42px (42/26 ≈ 1.615)
         'xl': ['2.625rem', { lineHeight: '1.2' }],    // 42px (h4)
         '2xl': ['4.5rem', { lineHeight: '1.16' }],    // 72px (h3)
-        '3xl': ['6.85rem', { lineHeight: '1', letterSpacing: '-0.02em' }],    // 109.7px (h2)
-        '4xl': ['13.3125rem', { lineHeight: '1', letterSpacing: '-0.02em' }], // 213px (h1)
+        '3xl': ['6.85rem', { lineHeight: '1', letterSpacing: '0.01em' }],    // 109.7px (h2)
+        '4xl': ['13.3125rem', { lineHeight: '0.84', letterSpacing: '-0.02em' }], // 213px (h1)
       },
     },
   },
