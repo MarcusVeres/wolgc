f28c1c7ad8664a0bba88c3851e13192733d25891
Marcus <marcus.veres@gmail.com>
2024-09-12 15:50:37 -0400

    adjusts properties of scrolling items, explodes class for scrolling container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+const XS = '320px';
+const SM = '480px';
+const MD = '540px';
+const LG = '640px';
+const XL = '768px';
+const XL2 = '900px';
+const XL3 = '1150px';
+const XL4 = '1400px';
+const XL5 = '1750px';
+const XL6 = '2150px';
+const XL7 = '2500px';
+const XL8 = '3750px';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,6 +24,16 @@ module.exports = {
       position: ['top', 'right'],
     },
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
       fontFamily: {
         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
@@ -24,43 +47,33 @@ module.exports = {
         '3xl': ['6.85rem', { lineHeight: '1', letterSpacing: '0.01em' }],    // 109.7px (h2)
         '4xl': ['13.3125rem', { lineHeight: '0.84', letterSpacing: '-0.02em' }], // 213px (h1)
       },
-      screens: {
-        'xs': '320px',
-        'sm': '480px',
-        'md': '540px',
-        'lg': '640px',
-        'xl': '768px',
-        '2xl': '1024px',
-        '3xl': '1280px',
-        '4xl': '1536px',
-        '5xl': '1800px',
-        '6xl': '1920px',
-        '7xl': '2560px',
-        '8xl': '3800px',
-      },
       maxWidth: {
-        'xs': '320px',
-        'sm': '480px',
-        'md': '540px',
-        'lg': '640px',
-        'xl': '768px',
-        '2xl': '1024px',
-        '3xl': '1280px',
-        '4xl': '1536px',
-        '5xl': '1800px',
-        '6xl': '1920px',
-        '7xl': '2560px',
-        '8xl': '3800px',
+        'xs': XS,
+        'sm': SM,
+        'md': MD,
+        'lg': LG,
+        'xl': XL,
+        '2xl': XL2,
+        '3xl': XL3,
+        '4xl': XL4,
+        '5xl': XL5,
+        '6xl': XL6,
+        '7xl': XL7,
+        '8xl': XL8,
       },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
-        },
+      screens: {
+        'xs': XS,
+        'sm': SM,
+        'md': MD,
+        'lg': LG,
+        'xl': XL,
+        '2xl': XL2,
+        '3xl': XL3,
+        '4xl': XL4,
+        '5xl': XL5,
+        '6xl': XL6,
+        '7xl': XL7,
+        '8xl': XL8,
       },
     },
   },
